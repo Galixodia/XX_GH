4e28a29bc7be035f2a6e03c9ebfff23f09b0aad1
Juan Camilo Villa P <jvillapa@gmail.com>
2021-02-14 09:43:23 -0500

    se agrega clase calculadora

--- src/gestionhumana/GestionHumana.java
+++ src/gestionhumana/GestionHumana.java
@@ -16,8 +16,19 @@ public class GestionHumana {
         Gusuarios.setXx_gh_usuarios_id(1);
         Gusuarios.setXx_gh_usuarios_nombre_com("Juan Camilo Villa");
         
+        XxGhUsuarios Gusuarios2 = new XxGhUsuarios(2, "Xiomara Torrez");
         
+        Calucladora c = new Calucladora();
+        
+        c.setA(10);
+        c.setB(0);
+        
+        
+                
+//        System.out.println(c.validarZero(0));
+        System.out.println(c.dividir());
         System.out.println(Gusuarios.toString());
+        System.out.println(Gusuarios2.toString());
     }
     
 }
